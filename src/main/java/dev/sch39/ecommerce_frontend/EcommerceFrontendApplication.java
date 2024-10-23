5a368e8c26f9cd9261dca91db3af1fa12e7791e8
Sch39 <sukronraezpactoer@gmail.com>
2024-10-23 08:09:33 +0700

    Init project

---
+++ src/main/java/dev/sch39/ecommerce_frontend/EcommerceFrontendApplication.java
@@ -0,0 +1,13 @@
+package dev.sch39.ecommerce_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sch39/ecommerce_frontend/EcommerceFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sch39.ecommerce_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
