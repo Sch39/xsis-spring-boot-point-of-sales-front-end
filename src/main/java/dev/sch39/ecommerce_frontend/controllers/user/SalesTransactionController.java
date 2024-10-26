e05d8975f1bc028a8ab630b976c930843a41ac4c
Sch39 <sukronraezpactoer@gmail.com>
2024-10-26 14:43:30 +0700

    Fix navbar and unused file

--- src/main/java/dev/sch39/ecommerce_frontend/controllers/user/SalesTransactionController.java
+++ src/main/java/dev/sch39/ecommerce_frontend/controllers/user/SalesTransactionController.java
@@ -1,14 +1,16 @@
 package dev.sch39.ecommerce_frontend.controllers.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("shop")
-public class ShopController {
+@RequestMapping("/sales-transaction")
+public class SalesTransactionController {
   @GetMapping({ "", "/" })
-  public String index() {
-    return "page/user/shop/index";
+  public String index(Model model) {
+    return "page/user/salestransaction/index";
   }
+
 }
