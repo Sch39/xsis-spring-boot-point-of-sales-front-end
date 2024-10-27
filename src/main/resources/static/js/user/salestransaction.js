236494b234cb5e025875913f2e34df603341f9cb
Sch39 <sukronraezpactoer@gmail.com>
2024-10-27 12:40:43 +0700

    Add selected data to localstorage and mark selected product result in search result

--- src/main/resources/static/js/user/salestransaction.js
+++ src/main/resources/static/js/user/salestransaction.js
@@ -1,4 +1,7 @@
-$('.quantity button').on('click', function () {
+const SELECTED_PRODUCTS_STORAGE_NAME='productsStorage';
+let productsStorage = [];
+
+$(document).on('click', '.quantity button', function () {
   var button = $(this);
   var oldValue = button.parent().parent().find('input').val();
   if (button.hasClass('btn-plus')) {
@@ -53,13 +56,20 @@ function truncateDescription(description, limit = 10) {
 
 function renderSearchResult(containerId, dataObj){
   $('#'+containerId).html('');
+  const products = JSON.parse(localStorage.getItem(SELECTED_PRODUCTS_STORAGE_NAME))||[];
 
   dataObj.forEach(item => {
+    const isSelected = products.some(product => product.id == item.id);
+
     $('#'+containerId).append(`
-    <tr>
+    <tr class="${isSelected ? 'text-muted': ''}">
       <td>
-        <button class="btn btn-sm text-dark p-2 btn-outline-dark" data-id="${item.id}">
-        <i class="fas fa-cart-plus text-primary mr-1"></i></button>
+        <button class="btn btn-sm text-dark p-2 btn-outline-dark add-product position-relative ${isSelected ? 'disabled' : ''}" data-id="${item.id}">
+        <i class="fas fa-cart-plus mr-1 ${isSelected ? 'text-info' : 'text-primary'}"></i>
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success ${isSelected ? '' : 'd-none'}">
+          <i class="fas fa-check mr-1"></i>
+        </span>
+        </button>
       </td>
       <td>
         <a href="/products/${item.slug}/detail" target="_blank" class="text-decoration-none">
@@ -67,9 +77,10 @@ function renderSearchResult(containerId, dataObj){
           <i class="fas fa-external-link-alt ml-1" title="Open in new tab"></i>
         </a>
       </td>
-      <td>${item.name}/
-      <br>
-      ${truncateDescription(item.description, 5)}
+      <td class="${isSelected ? 'text-decoration-line-through' : ''}">
+          ${item.name}/
+          <br>
+          ${truncateDescription(item.description, 5)}
       </td>
       <td>${item.price}</td>
       <td>${item.stock}</td>
@@ -185,9 +196,11 @@ function loadSearchResult(pageNumber=0, itemPerPage=5, sortBy='name', sortDirect
         console.log('success');
         
         const data = response.data.content;
+        productsStorage = data;
+
+        updatePagination(response.data.page);
         setTimeout(()=>{
           renderSearchResult('productTableBody', data);
-          updatePagination(response.data.page);
         }, 1500)
       } else {
         alert("Gagal memuat data!");
@@ -222,3 +235,65 @@ function updatePagination (pageInfo) {
   const page = $(this).data('page');
   loadSearchResult(page);
 });
+
+ $(document).on('click', '.add-product', function () {
+  const id = $(this).data('id');
+  const selectedProduct = productsStorage.find(product=> product.id===id);
+
+  let products = JSON.parse(localStorage.getItem(SELECTED_PRODUCTS_STORAGE_NAME)) || [];
+  const existingProducts = products.find(product=>product.id==id);
+
+  if (!existingProducts) {
+    products.push({
+      id: selectedProduct.id,
+      name: selectedProduct.name,
+      price: selectedProduct.price,
+      quantity: 1,
+    });
+  }
+  localStorage.setItem(SELECTED_PRODUCTS_STORAGE_NAME, JSON.stringify(products));
+
+  renderSearchResult('productTableBody', productsStorage);
+  updateCartDisplay();
+});
+
+function updateCartDisplay(){
+  const products = JSON.parse(localStorage.getItem(SELECTED_PRODUCTS_STORAGE_NAME)) ||[];
+
+  $('#selectedProductContainer').html('');
+
+  if (products.length == 0) {
+    $('#selectedProductContainer').append(`
+      <tr class="align-middle">
+              <td colspan="5" class="text-lg-center py-5">Belum ada barang dipilih!</td>
+      `);
+  }else{
+    products.forEach(product => {
+      $('#selectedProductContainer').append(`
+          <tr>
+                <td class="align-middle"><img src="https://picsum.photos/id/${product.item}/200/200" alt="" style="width: 50px;">
+                  ${product.name}
+                </td>
+                <td class="align-middle">${product.price}</td>
+                <td class="align-middle">
+                  <div class="input-group quantity mx-auto" style="width: 100px;">
+                    <button class="btn btn-sm btn-primary btn-minus">
+                      <i class="fa fa-minus"></i>
+                    </button>
+                    <input type="text" class="form-control form-control-sm bg-secondary text-center" value="${product.quantity}">
+                    <button class="btn btn-sm btn-primary btn-plus">
+                      <i class="fa fa-plus"></i>
+                    </button>
+                  </div>
+                </td>
+                <td class="align-middle">${product.price*product.quantity}</td>
+                <td class="align-middle"><button data-id="${product.id}" class="btn btn-sm btn-primary removeProduct"><i class="fa fa-times"></i></button></td>
+              </tr>
+        `);
+    });
+  }
+}
+
+window.onload=()=>{
+  updateCartDisplay();
+}
